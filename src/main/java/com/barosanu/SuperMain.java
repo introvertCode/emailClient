a327e076d5bb6e42e080895be2f7a522535c2314
introvertCode <stanislaw.bielski.programista@gmail.com>
2022-08-27 16:29:06 +0200

    jar, imges and finishing

---
+++ src/main/java/com/barosanu/SuperMain.java
@@ -0,0 +1,9 @@
+package com.barosanu;
+
+public class SuperMain {
+/* *********Class for maven shade plugin******** */
+    public static void main(String[] args) {
+        App.main(args);
+    }
+
+}
